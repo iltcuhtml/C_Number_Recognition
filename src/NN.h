76cfea69ec0a3127d2fcfa53b672a9e00c863f12
10119-JYC-Mario-O-O <toadchen83@gmail.com>
2025-05-03 19:02:01 +0900

    gates & matrix

--- src/NN.h
+++ src/NN.h
@@ -13,6 +13,8 @@
 #define NN_ASSERT assert
 #endif // NN_ASSERT
 
+float rand_float(void);
+
 typedef struct
 {
     size_t rows;
@@ -25,14 +27,22 @@ typedef struct
 
 Mat mat_alloc(size_t rows, size_t cols);
 
-void mat_dot(Mat dst, Mat m, Mat g);
-void mat_sum(Mat dst, Mat m);
+void mat_rand(Mat m, float low, float high);
+void mat_fill(Mat m, float x);
+
 void mat_print(Mat m);
+void mat_sum(Mat dst, Mat m);
+void mat_dot(Mat dst, Mat m, Mat g);
 
 #endif // NN_H
 
-#ifndef NN_IMPLEMENTATION
+#ifdef NN_IMPLEMENTATION
 #define NN_IMPLEMENTATION
+
+float rand_float(void)
+{
+    return (float) rand() / (float) RAND_MAX;
+}
 
 Mat mat_alloc(size_t rows, size_t cols)
 {
@@ -42,35 +52,70 @@ Mat mat_alloc(size_t rows, size_t cols)
     m.cols = cols;
     m.es = (float*) malloc(sizeof(*m.es) * rows * cols);
 
-    assert(m.es != NULL);
+    NN_ASSERT(m.es != NULL);
 
     return m;
 }
 
-void mat_dot(Mat dst, Mat m, Mat g)
+void mat_rand(Mat m, float low, float high)
 {
-    (void) dst;
-    (void) m;
-    (void) g;
+    for (size_t i = 0; i < m.rows; ++i)
+        for (size_t ii = 0; ii < m.cols; ++ii)
+        {
+            MAT_AT(m, i, ii) = rand_float() * (high - low) + low;
+        }
 }
 
-void mat_sum(Mat dst, Mat m)
+void mat_fill(Mat m, float x)
 {
-    (void) dst;
-    (void) m;
+    for (size_t i = 0; i < m.rows; ++i)
+        for (size_t ii = 0; ii < m.cols; ++ii)
+        {
+            MAT_AT(m, i, ii) = x;
+        }
 }
 
 void mat_print(Mat m)
 {
-    for (size_t row = 0; row < m.rows; ++row)
+    for (size_t i = 0; i < m.rows; ++i)
     {
-        for (size_t col = 0; col < m.cols; ++col)
+        for (size_t ii = 0; ii < m.cols; ++ii)
         {
-            printf("%f ", MAT_AT(m, row, col));
+            printf("%f ", MAT_AT(m, i, ii));
         }
 
         printf("\n");
     }
+}
+
+void mat_sum(Mat dst, Mat m)
+{
+    NN_ASSERT(dst.rows == m.rows);
+    NN_ASSERT(dst.cols == m.cols);
+
+    for (size_t i = 0; i < dst.rows; ++i)
+        for (size_t ii = 0; ii < dst.cols; ++ii)
+        {
+            MAT_AT(dst, i, ii) += MAT_AT(m, i, ii);
+        }
+}
+
+void mat_dot(Mat dst, Mat m, Mat g)
+{
+    NN_ASSERT(m.cols == g.rows);
+    NN_ASSERT(dst.rows == m.rows);
+    NN_ASSERT(dst.cols == g.cols);
+
+    for (size_t i = 0; i < dst.rows; ++i)
+        for (size_t ii = 0; ii < dst.cols; ++ii)
+        {
+            MAT_AT(dst, i, ii) = 0;
+
+            for (size_t iii = 0; iii < m.cols; ++iii)
+            {
+                MAT_AT(dst, i, ii) += MAT_AT(m, i, iii) * MAT_AT(g, iii, ii);
+            }
+        }
 }
 
 #endif // NN_IMPLEMENTATION
--- src/main.c
+++ src/main.c
@@ -1,10 +1,24 @@
+#include <time.h>
+
+#define NN_IMPLEMENTATION
 #include "NN.h"
 
 int main(void) // int argc, char* argv[]
 {
-    Mat m = mat_alloc(3, 4);
+    srand(time(0));
+
+    Mat m = mat_alloc(1, 2);
+    mat_rand(m, 5, 10);
+
+    Mat g = mat_alloc(2, 3);
+    mat_fill(g, 2);
+
+    Mat dst = mat_alloc(1, 3);
+    mat_dot(dst, m, g);
 
     mat_print(m);
-    
+    printf("------------------\n");
+    mat_print(dst);
+
     return 0;
 }
